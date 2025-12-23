2ed92fd294a278def0c71135f8a40582c04276af
Thinal Dewnayana <thinaldilmith2002@gmail.com>
2025-12-23 10:44:14 +0530

    fix ui

--- radio-one-ai/src/App.jsx
+++ radio-one-ai/src/App.jsx
@@ -1,12 +1,12 @@
 import Header from "./component/header";
 import Footer from "./component/footer";
+import Hero from "./pages/Hero/hero";
 
 export default function App() {
   return (
     <>
       <Header />
-     
-
+      <Hero />
       <Footer />
     </>
   );
