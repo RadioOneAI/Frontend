e543878599330498de4463d54a2f7e65216e9dd2
ShakyaPinnawala <shakyapinnawala@gmail.com>
2025-12-31 16:44:12 +0530

    register /admin/radiographers route in App.jsx

--- radio-one-ai/src/App.jsx
+++ radio-one-ai/src/App.jsx
@@ -9,7 +9,7 @@ import Login from "./pages/Auth/Login";
 import Signup from "./pages/Auth/Signup";
 import UserProfile from "./pages/UserProfile";
 import ContactUs from "./pages/ContactUs"; 
-import AboutUs from "./pages/AboutUs"; // <--- 1. IMPORT THIS
+import AboutUs from "./pages/AboutUs";
 
 // Admin Components
 import AdminLayout from "./pages/Admin/AdminLayout";
@@ -17,6 +17,7 @@ import DashboardHome from "./pages/Admin/DashboardHome";
 import Settings from "./pages/Admin/Settings"; 
 import ManageDoctors from "./pages/Admin/ManageDoctors"; 
 import ManageRadiologists from "./pages/Admin/ManageRadiologists"; 
+import ManageRadiographers from "./pages/Admin/ManageRadiographers"; // <--- 1. IMPORT ADDED
 import ManagePatients from "./pages/Admin/ManagePatients"; 
 import SystemLogs from "./pages/Admin/SystemLogs"; 
 
@@ -125,13 +126,14 @@ export default function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/profile" element={<UserProfile />} />
           <Route path="/contact" element={<ContactUs />} />
-          <Route path="/about" element={<AboutUs />} /> {/* <--- 2. ADD THIS ROUTE */}
+          <Route path="/about" element={<AboutUs />} />
 
           {/* --- Admin Routes --- */}
           <Route path="/admin/dashboard" element={<DashboardHome />} />
           <Route path="/admin/settings" element={<Settings />} />
           <Route path="/admin/doctors" element={<ManageDoctors />} />
           <Route path="/admin/radiologists" element={<ManageRadiologists />} />
+          <Route path="/admin/radiographers" element={<ManageRadiographers />} /> {/* <--- 2. ROUTE ADDED */}
           <Route path="/admin/patients" element={<ManagePatients />} />
           <Route path="/admin/logs" element={<SystemLogs />} />
 
